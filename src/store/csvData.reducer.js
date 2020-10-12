70306e3cef398228e1f2cc2b21d916b9e661838e
Maxwell Cohen <maxwell.cohen@globant.com>
2020-10-11 20:37:30 -0500

    improve aggregation

--- src/store/csvData.reducer.js
+++ src/store/csvData.reducer.js
@@ -1,10 +1,10 @@
 import * as Actions from './csvData.actions';
 import stateMap from '../constants/stateMap';
 import Calculations from '../utils/Calculations';
-import {camelCase} from 'lodash'
+import { camelCase } from 'lodash';
 
 const inital_state = {
-  titles: [], // list of keys that can be selected 
+  titles: [], // list of keys that can be selected
   rawData: [], // the parsed CSV file
   filteredData: [], // any filters run on the data (using filter funciton) also showen in table//
   stateKey: '',
@@ -12,65 +12,87 @@ const inital_state = {
   displayField: '',
   aggregationAction: '',
   filteringFuncitons: [],
-  mapData: [], //data grouped and agrated 
+  mapData: [], //data grouped and agrated
   loading: false,
 };
 
 export default (state = inital_state, action) => {
   switch (action.type) {
-    case Actions.REQUEST_DATA: 
-    return {
-      ...state,
-      loading: true
-    };
+    case Actions.REQUEST_DATA:
+      return {
+        ...state,
+        loading: true,
+      };
     case Actions.LOAD_DATA:
-    const  titles = action?.payload?.titles || [];
-    const rawData = action?.payload?.rawData || [];
-    const stateKey = titles.find((t) => normalizeState((rawData[0] || {})[t]) ) || '';
-    return {
-      ...inital_state,
-      rawData,
-      filteredData: rawData,
-      mapData: [],
-      titles,
-      stateKey
-    }
-    case Actions.SET_STATE_AND_GROUP: 
-    return {
-      ...state,
-      stateKey: action.payload.stateKey,
-      mapData: [],
-    }
-    case Actions.SET_DISPLAY: 
-    return {
-      ...state,
-      displayField: action.payload.displayField,
-      aggregationAction: action.payload.aggregationAction,
-      mapData: processToDisplay(state.filteredData, state.stateKey, action.payload.displayField, camelCase(action.payload.aggregationAction))
-    }
+      const titles = action?.payload?.titles || [];
+      const rawData = action?.payload?.rawData || [];
+      const stateKey =
+        titles.find((t) => normalizeState((rawData[0] || {})[t])) || '';
+      return {
+        ...inital_state,
+        rawData,
+        filteredData: rawData,
+        mapData: [],
+        titles,
+        stateKey,
+      };
+    case Actions.SET_STATE_AND_GROUP:
+      return {
+        ...state,
+        stateKey: action.payload.stateKey,
+        mapData: [],
+      };
+    case Actions.SET_DISPLAY:
+      //updateAaggregationAction
+      const aggerationOnly =
+        state.displayField &&
+        state.aggregationAction &&
+        action.payload.displayField === state.displayField;
+      return {
+        ...state,
+        displayField: action.payload.displayField,
+        aggregationAction: action.payload.aggregationAction,
+        mapData: aggerationOnly
+          ? updateAaggregationAction(
+              state.mapData,
+              action.payload.aggregationAction,
+            )
+          : processToDisplay(
+              state.filteredData,
+              state.stateKey,
+              action.payload.displayField,
+              action.payload.aggregationAction,
+            ),
+      };
     case Actions.ADD_FILTER_FN:
-    const existingFilters = state.filteringFuncitons.filter(({name})=> name !== action.payload.name);
-    const filteringFuncitons = [...existingFilters, action.payload];
-    const filteredData = filterData(state.rawData, filteringFuncitons)
-    return {
-      ...state,
-      filteringFuncitons,
-      filteredData,
-      mapData: processToDisplay(filteredData, state.stateKey, state.displayFunction)
-    }
-    case Actions.APPLY_FILTERS: 
-    return {
-      ...state,
-      mapData: action.payload.mapData,
-    }
+      const existingFilters = state.filteringFuncitons.filter(
+        ({ name }) => name !== action.payload.name,
+      );
+      const filteringFuncitons = [...existingFilters, action.payload];
+      const filteredData = filterData(state.rawData, filteringFuncitons);
+      return {
+        ...state,
+        filteringFuncitons,
+        filteredData,
+        mapData: processToDisplay(
+          filteredData,
+          state.stateKey,
+          state.displayField,
+          state.aggregationAction,
+        ),
+      };
+    case Actions.APPLY_FILTERS:
+      return {
+        ...state,
+        mapData: action.payload.mapData,
+      };
     default:
       return state;
   }
 };
 
-
 const groupData = (stateKey, data) => {
-  if(!stateKey) {
+  if (!stateKey) {
     return [];
   }
   const obj = data.reduce((acc, item) => {
@@ -78,7 +100,7 @@ const groupData = (stateKey, data) => {
       return acc;
     }
     const key = normalizeState(item[stateKey]);
-    if(!key) {
+    if (!key) {
       return acc;
     }
     if (!acc[key]) {
@@ -91,42 +113,57 @@ const groupData = (stateKey, data) => {
 };
 
 const normalizeState = (state) => {
-  if(typeof state !== 'string') {
+  if (typeof state !== 'string') {
     return null;
   }
-  const cleanState = stateMap[state.toLowerCase().replace(/[^a-z]/gmi,'')];
-  if(cleanState) {
+  const cleanState = stateMap[state.toLowerCase().replace(/[^a-z]/gim, '')];
+  if (cleanState) {
     return 'us-' + cleanState;
   }
   return null;
-}
-
+};
 
 export const filterData = (rawData, filters) => {
   if (filters.length === 0) {
     return rawData;
   }
   return rawData.filter((value) => {
-    const results = filters.map(({fn}) => fn(value)).filter(Boolean);
+    const results = filters.map(({ fn }) => fn(value)).filter(Boolean);
     return results.length === filters.length;
-  } )
-}
+  });
+};
 
-
-export const processToDisplay = (data, stateKey, displayField, aggregationAction) => {
+export const processToDisplay = (
+  data,
+  stateKey,
+  displayField,
+  aggregationAction,
+) => {
   if (!aggregationAction || !displayField) {
-    return []
+    return [];
   }
+
   const groupedData = groupData(stateKey, data);
   if (groupedData.length === 0) {
     return [];
   }
 
+  aggregationAction = camelCase(aggregationAction);
   return groupedData.map(([key, arr]) => {
-    const calc = new Calculations(arr, displayField)
-    console.log(calc);
+    const calc = new Calculations(arr, displayField);
     const displayVal = calc[aggregationAction]();
-    return [key, displayVal]
-  } )
+    return [key, displayVal, calc];
+  });
+};
 
+export const updateAaggregationAction = (groupedData, aggregationAction) => {
+  if (!aggregationAction) {
+    return [];
+  }
+
+  aggregationAction = camelCase(aggregationAction);
+  return groupedData.map(([key, df, calc]) => {
+    const displayVal = calc[aggregationAction]();
+    return [key, displayVal, calc];
+  });
 };
