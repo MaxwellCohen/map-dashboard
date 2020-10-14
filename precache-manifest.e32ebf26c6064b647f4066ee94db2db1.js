adf47982d64b355628634aee02bf319841629477
Maxwell Cohen <maxwell.cohen@globant.com>
2020-10-13 19:33:32 -0500

    Updates

---
+++ precache-manifest.e32ebf26c6064b647f4066ee94db2db1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2b40fbff27059d716e6e7939c62018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c25ee90599fbe5999917",
+    "url": "/static/css/2.70891016.chunk.css"
+  },
+  {
+    "revision": "ff6f7bad24abf3e2ddfb",
+    "url": "/static/css/main.8a958a44.chunk.css"
+  },
+  {
+    "revision": "c25ee90599fbe5999917",
+    "url": "/static/js/2.8ca33bae.chunk.js"
+  },
+  {
+    "revision": "d6d236f3e9a574dfc556f77bc3938f2f",
+    "url": "/static/js/2.8ca33bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6f7bad24abf3e2ddfb",
+    "url": "/static/js/main.84619496.chunk.js"
+  },
+  {
+    "revision": "495f61687abddd75d7b8",
+    "url": "/static/js/runtime-main.cb92a242.js"
+  }
+]);
