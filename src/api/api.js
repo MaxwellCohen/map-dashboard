f76078372e6aed5e4eb9b0396b578b9237f3a2e1
Maxwell Cohen <maxwell.cohen@globant.com>
2020-10-13 21:45:21 -0500

    update api to use my server

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 
 export const getCSV = (url) => {
+  const validURL = isValidHttpUrl(url);
+  if (!validURL) {
+    return Promise.reject('invalid url');
+  }
   return axios.get(
-    // `https://cors-anywhere.herokuapp.com/${url}`,
-     `http://localhost:8080/${url}`,
+     `https://map-dashboard-cors.herokuapp.com/${url}`,
+    //  `http://localhost:8080/${url}`,
     {
       headers: {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Headers":"Access-Control-Allow-Headers"}
     },
@@ -12,3 +16,14 @@ export const getCSV = (url) => {
 };
 
 
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
