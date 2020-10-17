4193a8941e74c5434fc6c8833c3766edaf590a7c
Maxwell Cohen <maxwell.cohen@globant.com>
2020-10-16 21:07:35 -0500

    Updates

---
+++ precache-manifest.7f2c8c7aeb9cad90a213abdd56b3dd9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d917d7e621bb0f528277f46cf6549e9d",
+    "url": "/map-dashboard/index.html"
+  },
+  {
+    "revision": "3eee32bcc481f69fe36a",
+    "url": "/map-dashboard/static/css/2.70891016.chunk.css"
+  },
+  {
+    "revision": "3eee32bcc481f69fe36a",
+    "url": "/map-dashboard/static/js/2.8fea73eb.chunk.js"
+  },
+  {
+    "revision": "d6d236f3e9a574dfc556f77bc3938f2f",
+    "url": "/map-dashboard/static/js/2.8fea73eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935794cd68d3e87873d9",
+    "url": "/map-dashboard/static/js/main.195da109.chunk.js"
+  },
+  {
+    "revision": "7d80ea47e30d899d6d28",
+    "url": "/map-dashboard/static/js/runtime-main.b69bbd5b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/map-dashboard/precache-manifest.a10e8323bf93b8a89a4dbb1cf1eb16ed.js"
+  "/map-dashboard/precache-manifest.7f2c8c7aeb9cad90a213abdd56b3dd9d.js"
 );
 
 self.addEventListener('message', (event) => {
